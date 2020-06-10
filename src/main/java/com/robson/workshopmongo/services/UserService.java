7007eb1c656e80459d0e7cb7c4c60e0f896b00aa
Robson Silva <rms2307@outlook.com>
2020-06-10 10:27:55 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/robson/workshopmongo/services/UserService.java
+++ src/main/java/com/robson/workshopmongo/services/UserService.java
@@ -25,14 +25,18 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
-	
+
 }
