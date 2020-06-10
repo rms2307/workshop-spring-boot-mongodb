14707ead660d03a82dec9840954c929495ad6f62
Robson Silva <rms2307@outlook.com>
2020-06-10 09:55:35 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/robson/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.robson.workshopmongo.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.robson.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice // Classe responsável por tratar possíveis erros nas requisições.
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
